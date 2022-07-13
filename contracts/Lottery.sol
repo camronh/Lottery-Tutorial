87c029505536cd962374c1584749e2065c75925b
camronh <camronhaider@gmail.com>
2022-07-13 14:59:24 -0400

    Starting tutorial

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,9 +1,4 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-// Import this file to use console.log
-import "hardhat/console.sol";
-
-contract Lottery {
-  
-}
+contract Lottery {}
--- test/Lottery.js
+++
@@ -1,7 +0,0 @@
-const { expect } = require("chai");
-
-
-describe("Lock", function () {
-    it("Should revert with the right error if called too soon", async function () { })
-
-})
